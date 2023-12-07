2345e66214cbc08ae8230c5608314fef83997984
Andrea Morabito <andrea.morabito@pagopa.it>
2023-12-07 23:26:35 +0100

    add day2 skeleton

---
+++ 2023/day2/src/main.rs
@@ -0,0 +1,19 @@
+fn main() {
+    println!("Hello, world!");
+}
+
+pub struct Game {
+    id: u8,
+    extractions: Vec<Extraction>
+}
+
+impl Game {
+    fn max_extraction() -> Extraction {
+        Extraction {
+
+        }
+    }
+}
+
+pub struct Extraction {}
+
