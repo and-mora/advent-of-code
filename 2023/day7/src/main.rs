caca3d780111434fec9764c8adde6513eacc3b3b
and-mora <andrea.morabito@pagopa.it>
2025-07-30 23:16:37 +0200

    fixed edge case in ordering and improve with idiomatic code

--- 2023/day7/src/main.rs
+++ 2023/day7/src/main.rs
@@ -16,47 +16,79 @@ enum Type {
     ThreeOfAKind,
     FullHouse,
     FourOfAKind,
-    FiveOfAKind
+    FiveOfAKind,
+}
+
+#[derive(Debug, Copy, Ord, PartialOrd, Eq, PartialEq, Hash, Clone)]
+enum Card {
+    Two,
+    Three,
+    Four,
+    Five,
+    Six,
+    Seven,
+    Eight,
+    Nine,
+    Ten,
+    Jack,
+    Queen,
+    King,
+    Ace,
+}
+
+impl From<char> for Card {
+    fn from(value: char) -> Self {
+        match value {
+            '2' => Card::Two,
+            '3' => Card::Three,
+            '4' => Card::Four,
+            '5' => Card::Five,
+            '6' => Card::Six,
+            '7' => Card::Seven,
+            '8' => Card::Eight,
+            '9' => Card::Nine,
+            'T' => Card::Ten,
+            'J' => Card::Jack,
+            'Q' => Card::Queen,
+            'K' => Card::King,
+            'A' => Card::Ace,
+            _ => panic!("Invalid card: {}", value),
+        }
+    }
 }
 
 #[derive(Debug)]
 pub struct Hand {
-    cards: Vec<char>,
+    cards: [Card; 5],
     bid: u32,
 }
 
 impl Hand {
-    pub fn new(cards_as_string: String, bid: u32) -> Self {
+    pub fn new(cards_as_string: &str, bid: u32) -> Result<Self, &str> {
         //validate input
         if cards_as_string.len() != 5 {
-            panic!("Hand must consist of 5 cards, got: {}", cards_as_string);
+            return Err("Hand must consist of 5 cards");
         }
-        if !cards_as_string.chars().all(|c| {
-            c.is_ascii_uppercase()
-                || c.is_digit(10)
-                || c == 'T'
-                || c == 'J'
-                || c == 'Q'
-                || c == 'K'
-                || c == 'A'
-        }) {
-            panic!(
-                "Hand must consist of uppercase letters or digits, got: {}",
-                cards_as_string
-            );
+        if !cards_as_string
+            .chars()
+            .all(|c| c.is_digit(10) || "TJQKA".contains(c))
+        {
+            return Err("Hand must consist of uppercase letters or digits");
         }
-        let cards: Vec<char> = cards_as_string.chars().collect();
-        Hand { cards, bid }
+        let cards = cards_as_string
+            .chars()
+            .map(|c| Card::from(c))
+            .collect::<Vec<Card>>()
+            .try_into()
+            .unwrap();
+        Ok(Hand { cards, bid })
     }
 
     fn type_of_hand(&self) -> Type {
-        let map_with_count = self
-            .cards
-            .iter()
-            .fold(HashMap::<char, u8>::new(), |mut acc, card| {
-                acc.entry(*card).and_modify(|v| *v += 1).or_insert(1);
-                acc
-            });
+        let map_with_count = self.cards.iter().fold(HashMap::new(), |mut acc, card| {
+            acc.entry(*card).and_modify(|v| *v += 1).or_insert(1);
+            acc
+        });
 
         if map_with_count.iter().map(|kv| *kv.1).max().unwrap() == 5 {
             return Type::FiveOfAKind;
@@ -89,8 +121,10 @@ impl Ord for Hand {
             .cmp(&other.type_of_hand())
             .then_with(|| {
                 for (index, card) in self.cards.iter().enumerate() {
-                    if card.cmp(&other.cards[index]) != Ordering::Equal {
-                        return card.cmp(&other.cards[index]);
+                    match card.cmp(&other.cards[index]) {
+                        Ordering::Greater => return Ordering::Greater,
+                        Ordering::Less => return Ordering::Less,
+                        Ordering::Equal => continue,
                     }
                 }
                 Ordering::Equal
@@ -123,7 +157,7 @@ impl Game {
 
     pub fn play(&self) -> u32 {
         let mut sorted_hands: Vec<&Hand> = self.hands.iter().clone().collect();
-        sorted_hands.sort_by(|a, b| b.cmp(a));
+        sorted_hands.sort();
         println!("Sorted hands: {:?}", sorted_hands);
         sorted_hands
             .iter()
@@ -139,12 +173,12 @@ impl Game {
 fn main() {
     println!("AOC 2023 day 7.");
 
-    let hand1 = Hand::new("32T3K".to_string(), 765);
-    let hand2 = Hand::new("T55J5".to_string(), 684);
-    let hand3 = Hand::new("KK677".to_string(), 28);
-    let hand4 = Hand::new("KTJJT".to_string(), 220);
-    let hand5 = Hand::new("QQQJA".to_string(), 483);
-    
+    let hand1 = Hand::new("32T3K", 765).unwrap();
+    let hand2 = Hand::new("T55J5", 684).unwrap();
+    let hand3 = Hand::new("KK677", 28).unwrap();
+    let hand4 = Hand::new("KTJJT", 220).unwrap();
+    let hand5 = Hand::new("QQQJA", 483).unwrap();
+
     let game = Game::new(vec![hand1, hand2, hand3, hand4, hand5]);
 
     println!("Game hands: {:?}", game.hands);
@@ -157,70 +191,70 @@ fn main() {
 #[cfg(test)]
 mod tests {
     use super::*;
-    
+
     #[test]
     fn test_hand_ordering_less() {
-        let hand1 = Hand::new("32T3K".to_string(), 0);
-        let hand2 = Hand::new("T55J5".to_string(), 0);
+        let hand1 = Hand::new("32T3K", 0);
+        let hand2 = Hand::new("T55J5", 0);
         assert_eq!(hand1 > hand2, false);
     }
 
     #[test]
     fn test_hand_ordering_second_criteria() {
-        let hand1 = Hand::new("3233K".to_string(), 0);
-        let hand2 = Hand::new("T55J5".to_string(), 0);
+        let hand1 = Hand::new("3233K", 0);
+        let hand2 = Hand::new("T55J5", 0);
         assert_eq!(hand1 > hand2, false);
     }
 
     #[test]
     fn test_hand_ordering_second_criteria_greater() {
-        let hand1 = Hand::new("QQQJA".to_string(), 0);
-        let hand2 = Hand::new("T55J5".to_string(), 0);
+        let hand1 = Hand::new("QQQJA", 0);
+        let hand2 = Hand::new("T55J5", 0);
         assert_eq!(hand1 > hand2, true);
     }
 
     #[test]
     fn test_hand_ordering_equal() {
-        let hand1 = Hand::new("T55J5".to_string(), 0);
-        let hand2 = Hand::new("T55J5".to_string(), 0);
+        let hand1 = Hand::new("T55J5", 0);
+        let hand2 = Hand::new("T55J5", 0);
         assert_eq!(hand1.cmp(&hand2), Ordering::Equal);
     }
 
     #[test]
     fn test_hand_ordering_greater() {
-        let hand1 = Hand::new("55555".to_string(), 0);
-        let hand2 = Hand::new("T55J5".to_string(), 0);
+        let hand1 = Hand::new("55555", 0);
+        let hand2 = Hand::new("T55J5", 0);
         assert_eq!(hand1.cmp(&hand2), Ordering::Greater);
     }
 
     #[test]
     fn test_hand_type_high_card() {
-        let hand = Hand::new("23456".to_string(), 684);
+        let hand = Hand::new("23456", 684).unwrap();
         assert_eq!(hand.type_of_hand(), Type::HighCard);
     }
     #[test]
     fn test_hand_type_one_pair() {
-        let hand = Hand::new("32T3K".to_string(), 765);
+        let hand = Hand::new("32T3K", 765).unwrap();
         assert_eq!(hand.type_of_hand(), Type::OnePair);
     }
     #[test]
     fn test_hand_type_two_pair() {
-        let hand = Hand::new("32T32".to_string(), 765);
+        let hand = Hand::new("32T32", 765).unwrap();
         assert_eq!(hand.type_of_hand(), Type::TwoPair);
     }
     #[test]
     fn test_hand_type_full_house() {
-        let hand = Hand::new("T55J5".to_string(), 684);
+        let hand = Hand::new("T55J5", 684).unwrap();
         assert_eq!(hand.type_of_hand(), Type::ThreeOfAKind);
     }
     #[test]
     fn test_hand_type_four_of_a_kind() {
-        let hand = Hand::new("K2KKK".to_string(), 765);
+        let hand = Hand::new("K2KKK", 765).unwrap();
         assert_eq!(hand.type_of_hand(), Type::FourOfAKind);
     }
     #[test]
     fn test_hand_type_five_of_a_kind() {
-        let hand = Hand::new("KKKKK".to_string(), 765);
+        let hand = Hand::new("KKKKK", 765).unwrap();
         assert_eq!(hand.type_of_hand(), Type::FiveOfAKind);
     }
 }
